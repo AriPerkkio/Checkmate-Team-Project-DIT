c77910922ac16819f0e87fe2ace4edec429d5e1a
AriPerkkio <t3pear00@students.oamk.fi>
2016-02-21 11:29:33 +0000

    Class created

---
+++ Checkmate/app/src/main/java/com/theateam/checkmate/GameController.java
@@ -0,0 +1,10 @@
+package com.theateam.checkmate;
+
+/**
+ * Created by AriPerkkio on 21/02/16.
+ *
+ * Class to control game logic. See class diagram for detailed info
+ *
+ */
+public class GameController {
+}
