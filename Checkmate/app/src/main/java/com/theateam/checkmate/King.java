0b67aae374b95af41c15e89ce60ac09dc2356dd7
AriPerkkio <t3pear00@students.oamk.fi>
2016-02-24 14:21:14 +0000

    Small changes

--- Checkmate/app/src/main/java/com/theateam/checkmate/King.java
+++ Checkmate/app/src/main/java/com/theateam/checkmate/King.java
@@ -4,4 +4,7 @@ package com.theateam.checkmate;
  * Created by arska on 24/02/16.
  */
 public class King extends Piece {
+
+    private boolean inCheck = false;
+    
 }
--- Checkmate/app/src/main/java/com/theateam/checkmate/Square.java
+++ Checkmate/app/src/main/java/com/theateam/checkmate/Square.java
@@ -19,4 +19,5 @@ public class Square {
     public boolean hasPiece(){
         return true; // TODO
     }
+
 }
