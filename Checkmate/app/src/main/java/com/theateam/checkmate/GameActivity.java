9329e492d10d78ecae5775f8a42ef26821137271
AriPerkkio <t3pear00@students.oamk.fi>
2016-02-24 14:50:34 +0000

    Hide navigation bar

--- Checkmate/app/src/main/java/com/theateam/checkmate/GameActivity.java
+++ Checkmate/app/src/main/java/com/theateam/checkmate/GameActivity.java
@@ -3,6 +3,7 @@ package com.theateam.checkmate;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -14,6 +15,11 @@ public class GameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //Set full screen
+        // Hide navigation bar and keep it hidden when pressing
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         coordinates = (TextView) findViewById(R.id.coordinateText);
