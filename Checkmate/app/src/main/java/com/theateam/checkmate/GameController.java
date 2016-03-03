4de3a80324f5e8a7665b0c6e0508ccea93372bc2
AriPerkkio <t3pear00@students.oamk.fi>
2016-03-03 18:57:19 +0000

    getValidMoves started - almost done

--- Checkmate/app/src/main/java/com/theateam/checkmate/GameController.java
+++ Checkmate/app/src/main/java/com/theateam/checkmate/GameController.java
@@ -43,42 +43,33 @@ public class GameController {
         return instance;
     }
 
-    public void movePiece(Piece _piece, Square from, Square target){
-        graphics.movePiece(_piece.getTextureId(), from.getId(), target.getId());
+    public void movePiece(Piece _piece, Square target, Square from){
+        graphics.movePiece(_piece.getTextureId(), target.getId(), from.getId());
+        from.setPiece(null);
+        _piece.setSquare(target);
     }
 
     // Boolean so that method can be broke if needed - see "return false" -statements
     public boolean selectSquare(String _square){
 
         // TODO: GameActivity.coordinates.setText -passing is here just in testing phase
-
+        //tests(_square);
         clickedSquare = _square;
 
-        if(clickedSquare.equals("OutOfBoard")) {
-            highlightsOff();
-            GameActivity.coordinates.setText(_square);
+        // Check if click is OutOfBoard, empty square or same as on the same piece as before
+        if(!checkSquare(clickedSquare)) // True means new piece was clicked
             return false;
-        }
 
-        // Check if there is a piece on the clicked square and enable it
-        if(board.getSquare(_square).getPiece()==null){
-            highlightsOff();
-            GameActivity.coordinates.setText("Square: "+ clickedSquare+ "\nNo Piece.");
-            return false;
-        }
+        // Get piece
+        selectedPiece = board.getSquare(clickedSquare).getPiece();
+        // Get valid moves
+        squareList.clear(); // Empty all moves
+        squareList = getValidMoves(selectedPiece);
+        // TODO JAMAL: Highlight squareList with learningTool's green X
 
-        // Check if previously clicked piece is the same one and disable it
-        if(selectedPiece != null && selectedPiece.equals(board.getSquare(_square).getPiece())){
-            highlightsOff();
-            GameActivity.coordinates.setText("Square: "+ clickedSquare+ "\nNo Piece.");
-            return true;
-        }
-
-        selectedPiece = board.getSquare(_square).getPiece();
         highlights.clear();
         highlights.add(new String[]{clickedSquare, "square", "green"});
         graphics.highlight(highlights);
-
 
 
         /** DEBUG **/
@@ -88,9 +79,34 @@ public class GameController {
                 "\nPiece: "+selectedPiece.getPlayer().toString()+"-"+
                 selectedPiece.getPieceType()+ " \nPieceTextureID: "+selectedPiece.getTextureId();
         GameActivity.coordinates.setText(printText);
-        //board.logBoardPrint();
+
+        board.logBoardPrint();
+
         /** DEBUG **/
 
+        return true;
+    }
+
+    public boolean checkSquare(String _square){
+        if(_square.equals("OutOfBoard")) {
+            highlightsOff();
+            GameActivity.coordinates.setText(_square);
+            return false;
+        }
+
+        // Check if there is a piece on the clicked square and enable it
+        if(board.getSquare(_square).getPiece()==null){
+            highlightsOff();
+            GameActivity.coordinates.setText("Square: "+ _square+ "\nNo Piece.");
+            return false;
+        }
+
+        // Check if previously clicked piece is the same one and disable it
+        if(selectedPiece != null && selectedPiece.equals(board.getSquare(_square).getPiece())){
+            highlightsOff();
+            GameActivity.coordinates.setText("Square: "+ _square+ "\nNo Piece.");
+            return false;
+        }
         return true;
     }
 
@@ -101,32 +117,47 @@ public class GameController {
         graphics.highlight(highlights);
     }
 
+    // TODO JAMAL: Check that movements are in A-H, 1-8. At the moment they go over the board
+    public List<Square> getValidMoves(Piece _piece){
+        List<Square> returnList = new Vector<>();
+        List<int[]> pieceMovements = _piece.getMovementList();
+        char column;
+        int row;
+        String fromSquare = _piece.getSquare().getId();
+
+        for(int i=0;i<pieceMovements.size();i++) {
+            column = (char) ((int) fromSquare.charAt(0) + pieceMovements.get(i)[0]);
+            row = Integer.parseInt(""+fromSquare.charAt(1)) + pieceMovements.get(i)[1];
+            Log.d("getValidMoves", column+""+row);
+            returnList.add(board.getSquare(column+""+row));
+        }
+        return returnList;
+    }
+
 
     // Adding tests here
     public void tests(String _square){
 
-        int pieceSelect = 31;
-        if(clickedSquare.equals("D2"))
-            graphics.movePiece(pieceSelect, "D3", "D2");
-        if(clickedSquare.equals("D3"))
-            graphics.movePiece(pieceSelect, "D4", "D3");
-        if(clickedSquare.equals("D4"))
-            graphics.movePiece(pieceSelect, "D5", "D4");
-        if(clickedSquare.equals("D5"))
-            graphics.movePiece(pieceSelect, "D6", "D5");
-        if(clickedSquare.equals("D6"))
-            graphics.movePiece(pieceSelect, "E6", "D6");
-        if(clickedSquare.equals("E6"))
-            graphics.movePiece(pieceSelect, "E5", "E6");
-        if(clickedSquare.equals("E5"))
-            graphics.movePiece(pieceSelect, "E4", "E5");
-        if(clickedSquare.equals("E4"))
-            graphics.movePiece(pieceSelect, "E3", "E4");
-        if(clickedSquare.equals("E3"))
-            graphics.movePiece(pieceSelect, "D3", "E3");
-        if(clickedSquare.equals("A1"))
+        if(_square.equals("D3")) {
+            if(selectedPiece!=null)
+                movePiece(selectedPiece, board.getSquare("D3"),board.getSquare("D2"));
+        }
+        if(_square.equals("D4")) {
+            if(selectedPiece!=null)
+                movePiece(selectedPiece, board.getSquare("D4"),board.getSquare("D3"));
+        }
+        if(_square.equals("D5")) {
+            if(selectedPiece!=null)
+                movePiece(selectedPiece, board.getSquare("D5"),board.getSquare("D4"));
+        }
+        if(_square.equals("D6")) {
+            if(selectedPiece!=null)
+                movePiece(selectedPiece, board.getSquare("D6"),board.getSquare("D5"));
+        }
+
+        if(_square.equals("A1"))
             graphics.rotate();
-        if(clickedSquare.equals("H5")) {
+        if(_square.equals("H5")) {
             graphics.eliminatePiece(46, "C7");
             graphics.eliminatePiece(47, "D7");
             graphics.eliminatePiece(48, "E7");
@@ -134,7 +165,7 @@ public class GameController {
         }
 
         // Learning tool tests
-        if(clickedSquare.equals("H4")){
+        if(_square.equals("H4")){
             highlights.clear();
             highlights.add(new String[]{"C7", "circle", "blue"});
             highlights.add(new String[]{"D7", "circle", "red"});
