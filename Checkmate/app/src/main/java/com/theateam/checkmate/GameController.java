455ddfdcb96404f6240dbf20f292c9519d94dcaa
AriPerkkio <t3pear00@students.oamk.fi>
2016-03-06 09:09:06 +0000

    getValidMovements moved to board to match class diagram

--- Checkmate/app/src/main/java/com/theateam/checkmate/GameController.java
+++ Checkmate/app/src/main/java/com/theateam/checkmate/GameController.java
@@ -33,20 +33,20 @@ public class GameController {
     private Piece selectedPiece;
     private boolean turn = true; // True indicates that it's playerOne's turn
 
-    private GameController(){
+    private GameController() {
         OpenGLRenderer.gameController = this;
         graphics = OpenGLRenderer.getInstance();
         playerTwo = new Player("Human", false); // Can be set as AI or human
         board = new Board(playerOne, playerTwo);
     }
 
-    public static GameController getInstance(){
-        if(instance == null)
+    public static GameController getInstance() {
+        if (instance == null)
             instance = new GameController();
         return instance;
     }
 
-    public void movePiece(Piece _piece, Square target, Square from){
+    public void movePiece(Piece _piece, Square target, Square from) {
         graphics.movePiece(_piece.getTextureId(), target.getId(), from.getId()); // Move piece in graphics
         from.setPiece(null); // Set old square empty
         _piece.setSquare(target); // Place piece to the new square
@@ -54,7 +54,7 @@ public class GameController {
 
     // Boolean so that method can be broke if needed - see "return false" -statements
     // Graphics are calling this method for every square click that is made
-    public boolean selectSquare(String _square){
+    public boolean selectSquare(String _square) {
 
         clickedSquare = _square; // update attribute - clickedSquare is used in other methods in this class
 
@@ -62,18 +62,18 @@ public class GameController {
         checkClickMovement();
 
         // Check if click is OutOfBoard, empty square or same as on the same piece as before
-        if(!checkSquare(clickedSquare)) // True means new piece was clicked
+        if (!checkSquare(clickedSquare)) // True means new piece was clicked
             return false; // Break function here
 
         // Get new piece
         selectedPiece = board.getSquare(clickedSquare).getPiece();
 
         // Check if clicked piece belongs to other player and break here
-        if(turn && selectedPiece.getPlayer().equals(playerTwo)){
+        if (turn && selectedPiece.getPlayer().equals(playerTwo)) {
             selectedPiece = null; // Reset selectedPiece back to null
             return false; // Break this function
         }
-        if(!turn && selectedPiece.getPlayer().equals(playerOne)){
+        if (!turn && selectedPiece.getPlayer().equals(playerOne)) {
             selectedPiece = null; // Reset selectedPiece back to null
             return false; // Break this function
         }
@@ -83,11 +83,11 @@ public class GameController {
 
         /** DEBUG **/
         // TODO: GameActivity.coordinates.setText -passing is here just in testing phase
-        Log.d("SelectedPiece",selectedPiece.getPlayer().toString()+""+selectedPiece.getPieceType()+ " ID: "+selectedPiece.getTextureId());
+        Log.d("SelectedPiece", selectedPiece.getPlayer().toString() + "" + selectedPiece.getPieceType() + " ID: " + selectedPiece.getTextureId());
         String printText =
-                "Square: "+ clickedSquare+
-                "\nPiece: "+selectedPiece.getPlayer().toString()+"-"+
-                selectedPiece.getPieceType()+ " \nPieceTextureID: "+selectedPiece.getTextureId();
+                "Square: " + clickedSquare +
+                        "\nPiece: " + selectedPiece.getPlayer().toString() + "-" +
+                        selectedPiece.getPieceType() + " \nPieceTextureID: " + selectedPiece.getTextureId();
         GameActivity.coordinates.setText(printText);
 
         board.logBoardPrint();
@@ -98,15 +98,14 @@ public class GameController {
     }
 
     // Check if new click was made to make selected piece to move or to eliminate a piece
-    public void checkClickMovement(){
-        if(selectedPiece!=null && board.getSquare(clickedSquare)!=null) { // Check that there was a piece selected and click is on the board
-            if (squareList.contains(board.getSquare(clickedSquare))){ // Check if clicked square was valid move to empty square
+    public void checkClickMovement() {
+        if (selectedPiece != null && board.getSquare(clickedSquare) != null) { // Check that there was a piece selected and click is on the board
+            if (squareList.contains(board.getSquare(clickedSquare))) { // Check if clicked square was valid move to empty square
                 movePiece(selectedPiece, board.getSquare(clickedSquare), selectedPiece.getSquare()); // Move piece in game logic and graphics
                 highlightsOff(); // Turn off all the highlights
                 turn = !turn;
                 checkRotating(); // Check if playerTwo is AI and rotate
-            }
-            else if(squareListTwo.contains(board.getSquare(clickedSquare))){ // Check if clicked square was valid capture movement
+            } else if (squareListTwo.contains(board.getSquare(clickedSquare))) { // Check if clicked square was valid capture movement
                 int tempPieceId = board.getSquare(clickedSquare).getPiece().getTextureId(); // Catch old piece id safe temporary
                 board.getSquare(clickedSquare).getPiece().remove(); // Eliminate old piece from game logic
                 movePiece(selectedPiece, board.getSquare(clickedSquare), selectedPiece.getSquare());  // Move piece to chosen square
@@ -119,28 +118,28 @@ public class GameController {
     }
 
     // Get valid moves and capture moves and highlight them
-    public void processMovements(){
+    public void processMovements() {
         squareList.clear(); // Empty all moves
         squareListTwo.clear();
-        squareList = getValidMoves(selectedPiece)[0]; // Valid moves
-        squareListTwo = getValidMoves(selectedPiece)[1]; // Valid capture moves
+        squareList = board.getValidMoves(selectedPiece)[0]; // Valid moves
+        squareListTwo = board.getValidMoves(selectedPiece)[1]; // Valid capture moves
 
         highlights.add(new String[]{clickedSquare, "square", "green"}); // Highlight clicked square
 
         // Highlight valid moves
-        for(int i=0;i<squareList.size();i++)
-            if(squareList.get(i)!=null)
+        for (int i = 0; i < squareList.size(); i++)
+            if (squareList.get(i) != null)
                 highlights.add(new String[]{squareList.get(i).getId(), "circle", "green"});
         // Highlight valid capture moves
-        for(int i=0;i<squareListTwo.size();i++)
-            if(squareListTwo.get(i)!=null)
+        for (int i = 0; i < squareListTwo.size(); i++)
+            if (squareListTwo.get(i) != null)
                 highlights.add(new String[]{squareListTwo.get(i).getId(), "cross", "red"});
         graphics.highlight(highlights);
     }
 
     // Check if clicked square was made to disable selected piece
-    public boolean checkSquare(String _square){
-        if(_square.equals("OutOfBoard")) {
+    public boolean checkSquare(String _square) {
+        if (_square.equals("OutOfBoard")) {
             selectedPiece = null; // Set as null, so that third press re-enables it again.
             highlightsOff();
             GameActivity.coordinates.setText(_square);
@@ -148,24 +147,24 @@ public class GameController {
         }
 
         // Check if there is a piece on the clicked square and enable it
-        if(board.getSquare(_square).getPiece()==null){
+        if (board.getSquare(_square).getPiece() == null) {
             selectedPiece = null; // Set as null, so that third press re-enables it again.
             highlightsOff();
-            GameActivity.coordinates.setText("Square: "+ _square+ "\nNo Piece.");
+            GameActivity.coordinates.setText("Square: " + _square + "\nNo Piece.");
             return false;
         }
 
         // Check if previously clicked piece is the same one and disable it
-        if(selectedPiece != null && selectedPiece.equals(board.getSquare(_square).getPiece())){
+        if (selectedPiece != null && selectedPiece.equals(board.getSquare(_square).getPiece())) {
             highlights.clear();
             selectedPiece = null;
             highlightsOff();
-            GameActivity.coordinates.setText("Square: "+ _square+ "\nNo Piece.");
+            GameActivity.coordinates.setText("Square: " + _square + "\nNo Piece.");
             return false;
         }
 
         // Check if previous click was on own piece, and new click on enemy piece that can't be captured
-        if(selectedPiece!=null && !selectedPiece.getPlayer().equals(board.getSquare(clickedSquare).getPiece().getPlayer())){
+        if (selectedPiece != null && !selectedPiece.getPlayer().equals(board.getSquare(clickedSquare).getPiece().getPlayer())) {
             highlights.clear();
             selectedPiece = null;
             highlightsOff();
@@ -176,59 +175,15 @@ public class GameController {
     }
 
     // Sets all the remaining highlighting textures as empty and calls graphics to show them
-    public void highlightsOff(){
+    public void highlightsOff() {
         String[] empty = new String[]{"hide", "empty", "empty"}; // Sets empty texture and empty coordinates
-        int count = 27-highlights.size(); // Get count for loop
-        for(int i=0;i<count;i++)
+        int count = 27 - highlights.size(); // Get count for loop
+        for (int i = 0; i < count; i++)
             highlights.add(empty);
         graphics.highlight(highlights);
         highlights.clear();
     }
 
-    // Returns [0] moves for empty squares, [1] movements for captures
-    public List<Square>[] getValidMoves(Piece _piece){
-        List<Square> returnListOne = new Vector<>(); // Holds movements for empty squares
-        List<Square> returnListTwo = new Vector<>(); // Holds capture movements
-        List<int[]> pieceMovements = _piece.getMovementList(); // Get pieces movements into int[]
-
-        char column; // As in 'A' - 'H'
-        int row; // 1-8
-        String fromSquare = _piece.getSquare().getId(); // Get piece's square
-        int direction = 0; // Used for pawns to indicate movement direction
-        if(!_piece.getPlayer().isFirst()) // Check if piece belongs to first player
-            direction = -2;
-
-        for(int i=0;i<pieceMovements.size();i++) { // Check all the movements
-            column = (char) ((int) fromSquare.charAt(0) + pieceMovements.get(i)[0]); // Calculate new column by adding initial column + movement's column
-            row = Integer.parseInt("" + fromSquare.charAt(1)) + pieceMovements.get(i)[1]; // Calculate new row same way as above
-            if(_piece.getPieceType().equals("Pawn")) // If it's pawn increase the row count by direction check number
-                row = row+direction;
-            if ((int) column >= (int) 'A' && (int) column <= (int) 'H' && row > 0 && row < 9) { // Check that new square is between A1-H8
-                Log.d("getValidMoves", column + "" + row);
-                if(board.getSquare(column + "" + row).getPiece()==null) // Check if square is empty
-                    returnListOne.add(board.getSquare(column + "" + row)); // Add empty square to list
-                else if(!board.getSquare(column + "" + row).getPiece().getPlayer().equals(selectedPiece.getPlayer())) // Check if square has enemy piece in it
-                    returnListTwo.add(board.getSquare(column + "" + row)); // Add enemy piece's square to other list
-            }
-        }
-        // For all the pieces capture movements are unique - so clear capture movements calculated above and get new ones
-        if(_piece.getPieceType().equals("Pawn")) {
-            returnListTwo.clear(); // Clear captures that were made above - pawns cant eliminate with [0,1] movement
-            List<int[]> extraCaptures = ((Pawn) _piece).getCaptureMovement(); // Cast piece to pawn and get capture movements
-            for (int i = 0; i < extraCaptures.size(); i++) { // Check all capture movements
-                column = (char) ((int) fromSquare.charAt(0) + extraCaptures.get(i)[0]); // Calculate new column
-                row = direction + Integer.parseInt("" + fromSquare.charAt(1)) + extraCaptures.get(i)[1]; // Calculate new row
-                if ((int) column >= (int) 'A' && (int) column <= (int) 'H' && row > 0 && row < 9) { // Check that square is valid
-                    Log.d("extraMoves", column + "" + row);
-                    // Check that square has enemy piece in it
-                    if(board.getSquare(column + "" + row).getPiece()!=null &&
-                      !board.getSquare(column + "" + row).getPiece().getPlayer().equals(selectedPiece.getPlayer()))
-                        returnListTwo.add(board.getSquare(column + "" + row)); // Add enemy piece's square to list
-                }
-            }
-        }
-        return new List[]{returnListOne, returnListTwo}; // return value [0] valid moves, [1] capture moves
-    }
 
     // Check if another player is not AI and does the rotating
     public void checkRotating() {
@@ -238,4 +193,3 @@ public class GameController {
         }
     }
 }
-
