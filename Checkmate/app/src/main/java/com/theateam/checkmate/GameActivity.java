322cb2e05f8226c470cbb2125f9de3353032a1f4
AriPerkkio <t3pear00@students.oamk.fi>
2016-04-04 14:56:20 +0100

    Copy chess AI engine from /res/raw to app dir

--- Checkmate/app/src/main/java/com/theateam/checkmate/GameActivity.java
+++ Checkmate/app/src/main/java/com/theateam/checkmate/GameActivity.java
@@ -1,15 +1,27 @@
 package com.theateam.checkmate;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class GameActivity extends Activity {
 
     //public static TextView coordinates;
     private GameController gameController = GameController.getInstance(); // At the moment this makes sure there is atleas one instance of gameController
+    private static GameActivity instance;
+    private static String directory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,61 @@ public class GameActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         //coordinates = (TextView) findViewById(R.id.coordinateText);
-
+        instance = this;
+        writeEngineToDevice();
+        directory = getFilesDir().toString()+"/engines/";
     }
-}
+    public static GameActivity getInstance(){
+        if(instance==null) Log.e("Instance", "null");
+        return instance;
+    }
+
+    public static String getDirectory(){
+        return directory;
+    }
+
+
+    private void writeEngineToDevice() {
+        directory = getApplicationContext().getFilesDir().toString()+"/engines/";
+
+        File engineDir = new File(directory);
+        if (!engineDir.exists()) {
+            Log.i("Writing", "Engine");
+            Log.i("Writing", "Directory"+directory);
+            engineDir.mkdirs();
+            copyEngineDir();
+        }
+        else
+            Log.d("writeEngineToDevice", "Directory exists");
+    }
+
+    // Write chess AI engine from /res/raw/ to applications directory
+    private void copyEngineDir() {
+        InputStream in;
+        OutputStream out;
+        try {
+            in = getResources().openRawResource(R.raw.stockfish); // Get stockfish raw binary file from /res/raw/
+            out = new FileOutputStream(directory + "stockfish"); // Create new file to applications directory
+            copyFile(in, out); // Copy file
+            in.close();
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            Log.e("copyEngineDir", e.toString());
+        }
+        try {
+            Runtime.getRuntime().exec("chmod 777 "+directory+"stockfish"); // Set executable
+        }catch(Exception e){
+            Log.e("Chmod777", e.toString());
+        }
+    }
+    // Reference: http://stackoverflow.com/a/5450828
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+ }
+
