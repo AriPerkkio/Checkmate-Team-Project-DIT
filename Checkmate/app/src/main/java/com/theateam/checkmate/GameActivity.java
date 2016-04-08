76ede28aff8fd80079389d1152081c1b2454c203
c13493258 <c13493258@gmail.com>
2016-04-08 15:51:38 +0100

    Chris found code to get navigation bar to disappear

--- Checkmate/app/src/main/java/com/theateam/checkmate/GameActivity.java
+++ Checkmate/app/src/main/java/com/theateam/checkmate/GameActivity.java
@@ -38,8 +38,13 @@ public class GameActivity extends Activity {
         // Hide navigation bar and keep it hidden when pressing
         /** Chris work here **/
         View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility(uiOptions);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
