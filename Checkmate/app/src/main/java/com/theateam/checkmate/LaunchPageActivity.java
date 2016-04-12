7b3fb11bf51e04d5b4ad74f7646e1afa1477e92a
AriPerkkio <t3pear00@students.oamk.fi>
2016-04-12 12:22:30 +0100

    temp button for previous games

--- Checkmate/app/src/main/java/com/theateam/checkmate/LaunchPageActivity.java
+++ Checkmate/app/src/main/java/com/theateam/checkmate/LaunchPageActivity.java
@@ -4,15 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class LaunchPageActivity extends AppCompatActivity {
 
     Button button;
+    private Button btnPrevGames;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //Set full screen
         setContentView(R.layout.activity_launch_page);
+
+        btnPrevGames = (Button) findViewById(R.id.btnPrevGames);
+        btnPrevGames.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LaunchPageActivity.this, PreviousGames.class);
+                LaunchPageActivity.this.startActivity(intent);
+            }
+        });
 
         button = (Button) findViewById(R.id.launchPageButton);
         button.setText("PLAY");
