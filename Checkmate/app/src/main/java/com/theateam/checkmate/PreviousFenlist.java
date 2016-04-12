49858e5af92a01ccb54144e01076c78fcc0b862f
AriPerkkio <t3pear00@students.oamk.fi>
2016-04-12 20:12:15 +0100

    Moving into different FEN working - comments later

--- Checkmate/app/src/main/java/com/theateam/checkmate/PreviousFenlist.java
+++ Checkmate/app/src/main/java/com/theateam/checkmate/PreviousFenlist.java
@@ -28,20 +28,30 @@ public class PreviousFenlist extends AppCompatActivity implements View.OnClickLi
     private ArrayList<String> fenList = new ArrayList<String>();
     private ArrayAdapter arrayAdapter;
     private int gameId;
+    private String gameMode;
+    private String learningTool;
+    private GameController gameController;
+    private static boolean status;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_previous_fenlist);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN); //Set full screen
 
-        btnBack = (Button) findViewById(R.id.btnPrevfenlistBack);
-        btnBack.setOnClickListener(this);
-        listFenlist = (ListView) findViewById(R.id.prevfenlistList);
-        listFenlist.setOnItemClickListener(this);
+        // Hide navigation bar and keep it hidden when pressing
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
+        status = true;
         gameId = getIntent().getExtras().getInt("GameId");
+        gameMode = getIntent().getExtras().getString("GameMode");
+        learningTool = getIntent().getExtras().getString("LearningTool");
 
         databaseManager = new DatabaseManager(this);
         try{
@@ -52,22 +62,39 @@ public class PreviousFenlist extends AppCompatActivity implements View.OnClickLi
             Log.e("previousFenlist", "Read DB, e: "+e);
         }
 
-        while(cursorFenlist.moveToNext())
+        do{
             fenList.add(cursorFenlist.getString(1));
+        }
+        while(cursorFenlist.moveToNext());
+
+        gameController = new GameController(gameMode, (learningTool.equals("ON")), fenList.get(0), fenList);
+        setContentView(R.layout.activity_previous_fenlist);
+
+        btnBack = (Button) findViewById(R.id.btnPrevfenlistBack);
+        btnBack.setOnClickListener(this);
+        listFenlist = (ListView) findViewById(R.id.prevfenlistList);
+        listFenlist.setOnItemClickListener(this);
 
         arrayAdapter = new ArrayAdapter<String>(this, R.layout.fenstringrow, fenList);
         listFenlist.setAdapter(arrayAdapter);
     }
 
+    // Indicates if this activity is on
+    public static boolean getStatus(){
+        return status;
+    }
+
     public void onClick(View v){
         switch(v.getId()){
             case R.id.btnPrevfenlistBack:
+                gameController = null;
+                status = false;
                 finish();
             break;
         }
     }
     public void onItemClick(AdapterView<?> adapterView, View v, int position, long arg){
-        Toast.makeText(this, "Clicked row "+position, Toast.LENGTH_SHORT).show();
+        gameController.previewFen(fenList.get(position));
     }
 
 
